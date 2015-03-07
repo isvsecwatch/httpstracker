33861eaa21a91efba2047462a2f5c91b6388cbdc
sindarina <sindarina@bbox.org>
2015-03-07 19:18:40 +0100

    Adding 3DES to the blacklist
    Previous version was actually an earlier version that only covered RC4.

--- google-chrome-launcher.sh
+++ google-chrome-launcher.sh
@@ -2,4 +2,4 @@
 #
 # This launches Google Chrome on OS X without support for RC4 and 3DES
 #
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --cipher-suite-blacklist=0x0004,0x0005,0xc011,0xc007 &
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --cipher-suite-blacklist=0x0004,0x0005,0xc011,0xc007,0x000A &
